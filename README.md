325f03e6d7a7dff31a74c7f16cc43b6c0ca0307e
jesseke55 <jessegeerts@hotmail.nl>
2015-08-21 17:05:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+You can use this plugin always but you can always help me or others to improve the code
+
+
+Friendly regards,
+Jesseke55
---
+++ src/xyz/jesseke55/AlwaysDay/Main.java
@@ -0,0 +1,47 @@
+package xyz.jesseke55.AlwaysDay;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+/**
+ * Created by jesse on 21-8-15.
+ */
+public class Main extends JavaPlugin implements Listener {
+
+
+    FileConfiguration config = getConfig();
+
+
+    public void onEnable(){
+        getLogger().info("-------------------------");
+        getLogger().info("");
+        getLogger().info("Name: " + getDescription().getName() );
+        getLogger().info("Version: " + getDescription().getVersion() );
+        getLogger().info("Authors: " + getDescription().getAuthors() );
+        getLogger().info("Website: " + getDescription().getWebsite() );
+        getLogger().info("");
+        getLogger().info(ChatColor.GREEN + "Please leave a rate if you like the plugin!");
+        getLogger().info("");
+        getLogger().info("-------------------------");
+
+
+        config.addDefault("world:", "world");
+
+        config.options().copyDefaults(true);
+        saveConfig();
+
+
+
+        BukkitScheduler scheduler = Bukkit.getScheduler();
+        scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+            public void run() {
+                Bukkit.getWorld(config.getString("world")).setTime(0L);
+            }
+        }, 60L, 60L);
+
+    }
+}
